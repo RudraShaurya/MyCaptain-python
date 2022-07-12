555c74db626639ff4cd0064f3a466866da9fb80e
RudraShaurya <108611424+RudraShaurya@users.noreply.github.com>
2022-07-12 15:40:13 +0530

    Add files via upload
    Positive Number in a range

---
+++ positive no..py
@@ -0,0 +1,18 @@
+list1 = []
+posi = []
+for i in range(1, 6):
+    n = int(input("Enter the elements of the list"))
+    list1.append(n)
+for j in list1:
+    if j >= 0:
+        posi.append(j)
+print(posi)
+list2 = []
+posi2 = []
+for i in range(1, 5):
+    p = int(input("Enter the elements of the list"))
+    list2.append(p)
+for j in list2:
+    if j >= 0:
+        posi2.append(j)
+print(posi2)
